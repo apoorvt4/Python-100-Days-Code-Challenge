e2fd529aba48a13b4966e61aeb2230a74641cb90
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-02 20:48:03 +0530

    f_string changes

---
+++ f_strings.py
@@ -0,0 +1,18 @@
+letter = "Hey my name is {0} and i am from {1}"
+country = "India"
+name = "Apoorv"
+
+# print(letter.format(name, country))
+
+print(f"Hey my name is {name} and I am from {country}")
+
+price = 49.0999
+txt = f"For only {price:.2f} dollars!"
+print(txt)
+
+brand = "Zara"
+price = 99.9999
+currency = "$"
+country_of_origin = "India"
+
+print(f"{brand} is an {country_of_origin} orign brand which cloths starts from this {price:.2f}{currency} pricing!")
