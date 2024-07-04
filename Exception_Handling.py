3ba01c84e75909ee97972086fd8163348021b7b9
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-04 11:41:01 +0530

    error

---
+++ Exception_Handling.py
@@ -0,0 +1,10 @@
+a = input("Enter the number: ")
+print(f"Multiplcation table of {a} is: ")
+
+
+try:
+    for i in range(1, 11):
+        print(f"{a} x {i} = {int(a)*i}")
+except Exception as e:
+    print("Invalid Input!") 
+        
