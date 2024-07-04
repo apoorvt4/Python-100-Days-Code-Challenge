c2f18f05e5df5d293b17d8a0517f2a2a7528ebf8
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-04 11:17:30 +0530

    Using else in for loops

---
+++ for_loops_with_else.py
@@ -0,0 +1,30 @@
+
+
+# This for for loop
+
+# for i in range(3):
+#     print(i)
+#     if i == 4:
+#         break
+# else:
+#     print("Sorry no i")
+
+
+# This for while loop
+
+# i = 0
+
+# while i<5:
+#     print(i)
+#     i = i + 1
+#     if i == 4:
+#         break
+# else:
+#     print("Sorry no i")
+
+
+for x in range(5):
+    print("iteration no {} in for loop".format(x+1))
+else:
+    print("else block in loop")
+print("out of loop")
