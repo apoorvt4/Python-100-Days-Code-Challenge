b90101ba536546d4e540453c06c8e428590c53c7
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-18 11:58:06 +0530

    Enumerate Function

---
+++ Enumerate_Function.py
@@ -0,0 +1,11 @@
+# marks = [12, 56, 32, 98, 12, 45, 1, 4]
+
+# for index, mark in enumerate(marks):
+#     print(mark)
+#     if(index == 3):
+#         print("Apoorv is awesome!")
+
+fruits = ["Apple", "bananna", "pineapple", "mango", "Cherry", "pappaya"]
+
+for index, fruit in enumerate(fruits, start=1):
+    print(index, fruit)
