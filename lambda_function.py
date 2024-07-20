6e177d5b0f5afb40f40ae81f6813048726c4db4a
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-20 15:22:18 +0530

    lambda

---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+# def double(x):
+#     return x*2
+
+# print(double(5))
+
+# def apple(dy, dx):
+#     return 6 + dy(dx)
+
+# double = lambda x: x*2
+# cube = lambda c: c*c*c
+# avg = lambda x, y, z: (x+y+z)/x*y*z
+
+# print(double(5))
+# print(cube(2))
+# print(avg(2,2,2))
+# print(apple(cube, 2))
+
+
