83b951fc7548df237c42d332cfb47706021bd279
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-20 17:23:49 +0530

    Game

---
+++ snake_water_gun_game.py
@@ -0,0 +1,28 @@
+# import random
+
+# def check(comp, user):
+#     if comp == user:
+#         return 0
+#     if(comp == 0 and user == 1):
+#         return -1
+#     if(comp == 1 and user == 2):
+#         return -1
+#     if(comp == 2 and user == 0):
+#         return -1
+#     return 1
+# comp = random.randint(0, 2)
+# user = int(input("0 for snake, 1 for water, 2 for Gun\n"))
+
+# print("You: ", user)
+# print("computer: ", user)
+
+# score = check(comp, user)
+# if(score == 0):
+#     print("Its a draw")
+# elif score == -1:
+#     print("You Lose!")
+# else:
+#     print("You Won!!!")
+
+
+
