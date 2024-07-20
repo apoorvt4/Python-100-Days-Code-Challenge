6027202c5abe86bfcd2ed1dc665e027fde110714
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-20 12:02:26 +0530

    pratices

--- Global_val.py
+++ Global_val.py
@@ -1,12 +1,31 @@
-y = 4
-print(y)
+# y = 4
+# print(y)
 
-def hello():
-    global  y
-    y = 10
-    x = 5
-    print(x)
-    print("Hello apoorv")
+# def hello():
+#     global  y
+#     y = 10
+#     x = 5
+#     print(x)
+#     print("Hello apoorv")
 
-hello()
-print(f"The global x is {y}")
+# hello()
+# print(f"The global x is {y}")
+
+# def apoorv_name():
+#     x = 5
+#     y = 10
+#     print(x + y)
+
+
+
+# apoorv_name()
+
+# def input_from_user():
+#     name = input("Enter Your number\n")
+#     jira = "Apoorv"
+
+#     for number in jira:
+#         if number in name:
+#             print(number)
+
+# input_from_user()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-apoorv.welcome()
+def input_from_user():
+    name = input("Enter Your number")
+    list = [2, 5, 7, 10]
+
+    if name is list():
+        print("You Guess it Write")
+    else:
+        print("NO Wrong Guess")
+
+input_from_user()
