f0d63fe684d08bdcce934d2992d024808d2eb242
apoorvt4 <apoorvtiwari292@gmail.com>
2024-07-22 19:58:36 +0530

    Getter and setter

--- Getters_and_setters.py
+++ Getters_and_setters.py
@@ -7,13 +7,13 @@
 #         return self._value
 
 
-class student:
-    greeting = "Welcome to the school"
-    __name = "Apoorv"
-    __age = "15"
-    __rollnumber = "CS4976445348"
-    def __init__(self):
-        print(self.greeting)
-        print(f"Student Name {self.__name}\n and there Age is {self.__age}\n and there connected Rollnumbers is {self.__rollnumber}")
+# class student:
+#     greeting = "Welcome to the school"
+#     __name = "Apoorv"
+#     __age = "15"
+#     __rollnumber = "CS4976445348"
+#     def __init__(self):
+#         print(self.greeting)
+#         print(f"Student Name {self.__name}\n and there Age is {self.__age}\n and there connected Rollnumbers is {self.__rollnumber}")
 
-obj = student()
+# obj = student()
---
+++ OOPS_Introduction.py
@@ -0,0 +1,19 @@
+def hello():
+    print("hello")
+
+hello()
+
+sales = 6000
+profit = 2000
+ad = 1000
+# rajeev.sales
+
+
+sales2 = 6000
+profit2 = 2000
+ad2 = 1000
+# vikrant.sales
+
+sales3 = 6000
+profit3 = 2000
+ad3 = 1000
