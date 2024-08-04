518ccb63af0c63e39b501f26be6f22484a9c1742
apoorvt4 <apoorvtiwari292@gmail.com>
2024-08-04 18:10:13 +0530

    Hybrid Inheritance

---
+++ Hybrid_Inheritence.py
@@ -0,0 +1,17 @@
+class Base:
+    pass
+
+class Derive(Base):
+    pass
+
+class Derive1(Base):
+    pass
+
+class Derive2(Base):
+    pass
+
+class Derive3(Base):
+    pass
+
+class Intermidiate(Derive3):
+    pass
