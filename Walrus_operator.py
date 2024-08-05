94bcc7ff212178be72f5c4ea233f315947cbbc84
apoorvt4 <apoorvtiwari292@gmail.com>
2024-08-05 14:23:27 +0530

    walrus operator

---
+++ Walrus_operator.py
@@ -0,0 +1,24 @@
+# a = True
+# print(a:=False)
+
+# numbers = [1, 2, 3, 4, 5]
+
+# while(n:= len(numbers)) > 0:
+#     print(numbers.pop())
+
+
+# This is  aam Jindigi
+# foods = list()
+
+# while True:
+#     food = input("What food do you like?\n")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+# This is mintos Jindigi
+
+foods = list()
+
+while (food:= input("What food do you like:"))!="quit":
+    foods.append(food)
