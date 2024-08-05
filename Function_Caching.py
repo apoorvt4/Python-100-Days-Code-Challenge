cbfae5f5aec6ba691f0569c4280935fc3f1379f2
apoorvt4 <apoorvtiwari292@gmail.com>
2024-08-05 16:29:22 +0530

    cashing method

---
+++ Function_Caching.py
@@ -0,0 +1,29 @@
+from functools import lru_cache
+import time
+lru_cache(maxsize=None)
+# def fx(n):
+#     time.sleep(5)
+#     return n*5
+
+# print(fx(20))
+# print("done for 20")
+# print(fx(2))
+# print("done for 2")
+# print(fx(6))
+# print("done for 6")
+
+# print(fx(20))
+# print("done for 20")
+# print(fx(2))
+# print("done for 2")
+# print(fx(6))
+# print("done for 6")
+
+def sum(a):
+    time.sleep(2)
+    return a + 2
+def sums(b):
+    return sum + b
+
+print(sum(2))
+
